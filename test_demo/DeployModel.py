df5424fc8c004694ee72a2f7b81b924822f81698
tamasjambor <jamborta@gmail.com>
2019-02-28 15:40:31 +0000

    update test demo

--- test_demo/DeployModel.py
+++ test_demo/DeployModel.py
@@ -1,5 +1,6 @@
 import os
 import pickle
+from myelin import metric
 
 model_path = os.environ.get('MODEL_PATH') or '/tmp/model/'
 
@@ -8,15 +9,15 @@ class DeployModel(object):
 
 	def __init__(self):
 		self.model = pickle.load(open(model_path + "lr.pkl", 'rb'))
+		self.c = metric.MetricClient()
 
 	def predict(self, X, feature_names):
 		predictions = self.model.predict(X)
 		return predictions
 
 	def send_feedback(self, features, feature_names, reward, truth):
-		pass
+		self.c.post_update("deploy_accuracy", reward)
 
 
 if __name__ == '__main__':
 	d = DeployModel()
-	print(d.predict([[1, 2, 3], [2, 3, 4]], None))
