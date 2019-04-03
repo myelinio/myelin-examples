cf12dec1eeebce311d872d910708f97e6f6e95a0
Ryadh Khsib <ryadh.khsib@gmail.com>
2019-04-03 20:06:26 +0100

    HP Tuning test using Myelin client.

--- test_demo_hp/train.py
+++ test_demo_hp/train.py
@@ -11,7 +11,7 @@ from sklearn.metrics import mean_squared_error
 logging.basicConfig(level=logging.DEBUG)
 
 
-class SKlearnWorker():
+class DemoHPWorker():
 
     def compute(self, config_id, kernel, C, epsilon, budget):
         config = {
@@ -44,30 +44,10 @@ class SKlearnWorker():
         print("rmse: %s" % rmse)
 
         info_map = {'train rmse': rmse}
-        post_result(config_id, config, budget, rmse, info_map)
 
         metric_client = MetricClient()
         metric_client.post_update("rmse", rmse)
-
-
-def post_result(config_id, config, budget, loss, info_map):
-    train_controller_url = os.environ['TRAIN_CONTROLLER_URL']
-    logging.info("train_controller_url: %s" % train_controller_url)
-
-    result_dict = ({
-        'loss': loss,
-        'info': info_map
-    })
-    result = {'result': result_dict, 'exception': None}
-    res_post = {'result': result, 'budget': budget, 'config_id': build_config_id(config_id), 'config': config}
-    response = requests.post("%s/submit_result" % train_controller_url, json=res_post)
-    print('response: %s' % response.status_code)
-    if response.status_code != 200:
-        raise Exception("reporting HP failed, error: %s" % response.text)
-
-
-def build_config_id(config_id):
-    return [int(x) for x in config_id.split("_")]
+        metric_client.post_result(config_id, config, budget, rmse, info_map)
 
 
 def main():
@@ -79,11 +59,11 @@ def main():
     parser.add_argument('--budget', type=float)
 
     args = parser.parse_args()
-    SKlearnWorker().compute(args.config_id,
-                            args.kernel,
-                            args.C,
-                            args.epsilon,
-                            args.budget)
+    DemoHPWorker().compute(args.config_id,
+                           args.kernel,
+                           args.C,
+                           args.epsilon,
+                           args.budget)
 
 
 if __name__ == '__main__':
