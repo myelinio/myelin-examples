ea44d0a5dd5d363c99ba79c4c3192176d018b98e
Ryadh Khsib <ryadh.khsib@gmail.com>
2019-04-26 14:05:55 +0100

    OCR Web demo.

--- ocr/web_demo/predict_web.py
+++ ocr/web_demo/predict_web.py
@@ -9,6 +9,8 @@ import numpy as np
 import requests
 
 import imageio
+from flask.json import jsonify
+from flask_cors import CORS, cross_origin
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
@@ -34,12 +36,11 @@ def get_host_info():
     return ret
 
 
-def detect_text(image):
-    url = 'http://localhost:5000/predict'
+def detect_text(image, model_url):
     jsondata = json.dumps({"data": {"ndarray": image.tolist()}})
     payload = {'json': jsondata}
     session = requests.session()
-    response = session.post(url, data=payload, headers={'User-Agent': 'test'})
+    response = session.post(model_url, data=payload, headers={'User-Agent': 'test'})
 
     print("Response code: %s" % response.status_code)
     json_data = json.loads(response.text)
@@ -47,12 +48,44 @@ def detect_text(image):
     return prediction
 
 
+def detect_text_from_request():
+    bio = io.BytesIO()
+    request.files['image'].save(bio)
+    img = cv2.imdecode(np.frombuffer(bio.getvalue(), dtype='uint8'), 1)
+    model_url = 'http://localhost:5000/predict'
+
+    return detect_text(img, model_url)
+
+
 app = Flask(__name__)
+CORS(app)
 
 
 @app.route('/')
 def index():
     return render_template('index.html', session_id='dummy_session_id')
+
+
+@app.route('/api/models')
+def get_modes():
+    return
+
+
+@app.route('/api/detect', methods=['POST'])
+def detect():
+    bio = io.BytesIO()
+    request.files['image'].save(bio)
+    img = cv2.imdecode(np.frombuffer(bio.getvalue(), dtype='uint8'), 1)
+    session_id = str(uuid.uuid1())
+    prediction = detect_text(img, 'http://localhost:5000/predict')
+    imageio.imwrite("%s/%s_output_predict.jpg" % (save_dir, session_id), prediction['img_drawed'])
+
+    response = [{
+        'title': session_id,
+        'source': "http://0.0.0.0:8769/%s/%s_output_predict.jpg" % (save_dir, session_id),
+    }]
+
+    return jsonify(response)
 
 
 @app.route('/', methods=['POST'])
@@ -61,7 +94,7 @@ def index_post():
     request.files['image'].save(bio)
     img = cv2.imdecode(np.frombuffer(bio.getvalue(), dtype='uint8'), 1)
     session_id = str(uuid.uuid1())
-    prediction = detect_text(img)
+    prediction = detect_text(img, 'http://localhost:5000/predict')
     imageio.imwrite("%s/%s_output_predict.jpg" % (save_dir, session_id), prediction['img_drawed'])
 
     rst = {
