e8d86167b3a87438c9fe12062c74167662a5ee98
Ryadh Khsib <ryadh.khsib@gmail.com>
2019-06-01 00:49:52 +0100

    dynamic model_key loading.

--- test_demo_dag/model3/model.py
+++ test_demo_dag/model3/model.py
@@ -8,22 +8,28 @@ model_path = os.environ.get('MODEL_PATH') or '/tmp/model/'
 
 class DeployModel3(object):
 
-	def __init__(self):
-		self.model = pickle.load(open(model_path + "lr.pkl", 'rb'))
-		self.c = metric.MetricClient()
+    def __init__(self):
+        self.model = pickle.load(open(model_path + "lr.pkl", 'rb'))
+        self.c = metric.MetricClient()
 
-	def predict(self, features_dict):
-		x_model1 = features_dict['1']
-		x_model2 = features_dict['2']
-		x_input = features_dict['INPUT']
-		X = np.concatenate([[x_model1], [x_model2], x_input], axis=1)
-		predictions = self.model.predict(X)
-		return predictions
+    def predict(self, features_dict):
+        x_model1 = features_dict[self.model_key(features_dict, 'model-graph-deploymodel1')]
+        x_model2 = features_dict[self.model_key(features_dict, 'model-graph-deploymodel2')]
+        x_input = features_dict['INPUT']
+        X = np.concatenate([[x_model1], [x_model2], x_input], axis=1)
+        predictions = self.model.predict(X)
+        return predictions
 
-	def send_feedback(self, features, feature_names, reward, truth):
-		res = self.c.post_update("deploy_accuracy", reward)
-		print("Posted metric with status code: %s" % res.status_code)
+    def send_feedback(self, features, feature_names, reward, truth):
+        res = self.c.post_update("deploy_accuracy", reward)
+        print("Posted metric with status code: %s" % res.status_code)
+
+    def model_key(self, features_dict, model_name):
+        for k, _ in features_dict.items():
+            if k.startswith(model_name):
+                return k
+        raise Exception("model %s not found in features keys %s" % (model_name,))
 
 
 if __name__ == '__main__':
-	d = DeployModel3()
+    d = DeployModel3()
