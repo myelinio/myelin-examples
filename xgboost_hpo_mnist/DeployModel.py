4565f166eebc6e59c0ea68fb1dd111e0830b4f79
khsibr <ryadh.khsib@gmail.com>
2019-11-22 15:04:39 +0000

    xgboost hp tuning.

---
+++ xgboost_hpo_mnist/DeployModel.py
@@ -0,0 +1,24 @@
+import os
+import pickle
+from myelin import metric
+
+model_path = os.environ.get('MODEL_PATH') or '/tmp/model/'
+
+
+class DeployModel(object):
+
+	def __init__(self):
+		self.model = pickle.load(open(model_path + "sk.pkl", 'rb'))
+		self.c = metric.MetricClient()
+
+	def predict(self, X, feature_names):
+		predictions = self.model.predict(X)
+		return predictions
+
+	def send_feedback(self, features, feature_names, reward, truth):
+		res = self.c.post_update("deploy_accuracy", reward)
+		print("Posted metric with status code: %s" % res.status_code)
+
+
+if __name__ == '__main__':
+	d = DeployModel()
