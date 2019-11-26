84dd36aea26343b11262f4493de7b4ad41a783c4
khsibr <ryadh.khsib@gmail.com>
2019-11-26 18:56:43 +0000

    Spark PI example.

--- pi-example/copyArtifact.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-gcloud auth activate-service-account --key-file=/var/secrets/google/spark-sa.json spark-bq@myelin-development.iam.gserviceaccount.com
-gsutil cp ./pi-example-1.0-SNAPSHOT-jar-with-dependencies.jar gs://myelin-development-spark-on-k8s2/jars/
-
-exec "$@"
