89d4b50d7707a181837f3027bfafff18cb4dc986
tamasjambor <jamborta@gmail.com>
2020-03-14 20:05:58 +0000

    update test cases

---
+++ tests/test-demo/DeployModel.py
@@ -0,0 +1,32 @@
+import os
+import pickle
+from myelin import metric
+import myelin.admin
+
+model_path = os.environ.get('MODEL_PATH') or '/tmp/model/'
+task_id = os.environ.get('TASK_ID')
+train_task = myelin.admin.task(task_name="TrainMyelinTest")
+print(train_task)
+assert train_task.model_path == model_path
+
+
+class DeployModel(object):
+
+	def __init__(self):
+		self.model = pickle.load(open(model_path + "lr.pkl", 'rb'))
+		self.c = metric.MetricClient()
+
+	def predict(self, X, feature_names):
+		predictions = self.model.predict(X)
+		return predictions
+
+	def send_feedback(self, features, feature_names, reward, truth):
+		res = self.c.post_update("deploy_accuracy", reward)
+		print("Posted metric with status code: %s" % res.status_code)
+
+	def tags(self):
+		return {'task_id': task_id}
+
+
+if __name__ == '__main__':
+	d = DeployModel()
---
+++ tests/test-demo/train.py
@@ -0,0 +1,30 @@
+import os
+from sklearn import linear_model
+import pandas as pd
+import pickle
+import myelin.metric
+import myelin.admin
+
+data_path = os.environ.get('DATA_PATH') or '/tmp/data/'
+model_path = os.environ.get('MODEL_PATH') or '/tmp/model/'
+
+prep_task = myelin.admin.task(task_name="DataPrepTest")
+print(prep_task)
+assert prep_task.data_path == data_path
+
+if not os.path.exists(model_path):
+	os.makedirs(model_path)
+
+print("Loading data")
+df = pd.read_pickle(data_path + "example.pkl")
+
+X = df[["B", "C", "D"]].values
+Y = df.A
+
+print("Training model")
+regr = linear_model.LinearRegression()
+regr.fit(X, Y)
+print('Coefficients: \n', regr.coef_)
+
+pickle.dump(regr, open(model_path + "lr.pkl", 'wb'))
+myelin.metric.publish_result(0.9, "test_accuracy")
