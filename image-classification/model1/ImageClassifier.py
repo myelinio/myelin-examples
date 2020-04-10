32170becc37ca7f2111f9762df6b834dc80f25eb
tamasjambor <jamborta@gmail.com>
2020-04-10 23:25:47 +0100

    image classifer example

---
+++ image-classification/model1/ImageClassifier.py
@@ -0,0 +1,20 @@
+import os
+import keras
+from myelin import metric
+
+model_path = os.environ.get('MODEL_PATH') or '/tmp/model/'
+
+
+class ImageClassifier(object):
+
+	def __init__(self):
+		self.model = keras.models.load_model(os.path.join(model_path, "saved_models/keras_cifar10_trained_model.h5"))
+		self.c = metric.MetricClient()
+
+	def predict(self, X, feature_names):
+		predictions = self.model.predict(X)
+		return predictions
+
+	def send_feedback(self, features, feature_names, reward, truth):
+		res = self.c.post_update("image_classifer_accuracy", reward)
+		print("Posted metric with status code: %s" % res.status_code)
