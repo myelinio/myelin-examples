ea897ff9f56ec8f24133fcc51131b5923bff4c05
khsibr <ryadh.khsib@gmail.com>
2020-04-18 21:37:22 +0100

    publish accuracy.

--- dist-mnist/DeployModel.py
+++ dist-mnist/DeployModel.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 import logging
 import os
+import glob
 
 logging.basicConfig(format='%(asctime)s.%(msecs)03d %(levelname)s {%(module)s} [%(funcName)s] %(message)s',
                     datefmt='%Y-%m-%d,%H:%M:%S', level=logging.INFO)
@@ -10,10 +11,15 @@ logger = logging.getLogger(__name__)
 class DeployModel(object):
     def __init__(self):
         self.class_names = ["class:{}".format(str(i)) for i in range(10)]
-        self.sess = tf.Session()
+        session_config = tf.ConfigProto(allow_soft_placement=True)
+        self.sess = tf.Session(config=session_config)
+
         model_path = os.path.join(os.getenv('MODEL_PATH', '/tmp'), 'model')
 
-        saver = tf.train.import_meta_graph(os.path.join(model_path, 'model.meta'))
+        meta_files = glob.glob(os.path.join(model_path, '*.meta'))
+        meta_files.sort(key=os.path.getmtime)
+
+        saver = tf.train.import_meta_graph(meta_files[-1])
         saver.restore(self.sess, tf.train.latest_checkpoint(model_path))
 
         graph = tf.get_default_graph()
@@ -32,19 +38,19 @@ if __name__ == '__main__':
     import numpy as np
 
     #
-    data_dir = os.path.join(os.getenv('DATA_PATH', '/tmp'), 'tensorflow/mnist/logs/mnist_with_summaries/train')
+    data_dir = os.path.join(os.getenv('DATA_PATH', '/tmp'), 'model')
     mnist = input_data.read_data_sets(data_dir)
     batch = mnist.train.next_batch(10)
-    # d = DeployModel()
-    # x_train = batch[0]
-    # print(d.predict(x_train, {}), batch[1])
-    url = "http://localhost:8080/predict"
-    session = requests.session()
-    response = session.post(url, json={"data": {"ndarray": batch[0].tolist()}}, headers={'User-Agent': 'test'})
-
-    print("response.status_code: ", response.status_code)
-    print("response.text", response.text)
-    json_data = json.loads(response.text)
-    prediction = json_data["data"]["ndarray"]
+    d = DeployModel()
+    x_train = batch[0]
+    print(d.predict(x_train, {}), batch[1])
+    # url = "http://localhost:8080/predict"
+    # session = requests.session()
+    # response = session.post(url, json={"data": {"ndarray": batch[0].tolist()}}, headers={'User-Agent': 'test'})
+    #
+    # print("response.status_code: ", response.status_code)
+    # print("response.text", response.text)
+    # json_data = json.loads(response.text)
+    # prediction = json_data["data"]["ndarray"]
 
     print("predicted class: %s" % np.argmax(np.array(prediction)))
