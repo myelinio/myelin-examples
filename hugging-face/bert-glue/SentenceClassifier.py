cfbc2c9614a78765f889ed35f570cd97ac5a0699
tamasjambor <jamborta@gmail.com>
2020-04-23 15:21:04 +0100

    fix predictor

--- hugging-face/bert-glue/SentenceClassifier.py
+++ hugging-face/bert-glue/SentenceClassifier.py
@@ -7,15 +7,15 @@ from transformers import (
 model_path = os.environ.get('MODEL_PATH') or '/tmp/model/'
 
 
-class SentenceClassifier():
+class SentenceClassifier(object):
 
 	def __init__(self):
 		self.model = TFBertForSequenceClassification.from_pretrained(os.path.join(model_path, "save/"))
 		self.tokenizer = BertTokenizer.from_pretrained("bert-base-cased")
 
 	def predict(self, X, feature_names):
-		s1 = X[0]
-		s2 = X[1]
-		inputs = self.tokenizer.encode_plus(s1, s2, add_special_tokens=True, return_tensors="pt")
-		pred = self.model(**inputs)[0].argmax().item()
+		sentence1 = X[0]
+		sentence2 = X[1]
+		inputs = self.tokenizer.encode_plus(sentence1, sentence2, add_special_tokens=True, return_tensors='tf')
+		pred = self.model(inputs['input_ids'], token_type_ids=inputs['token_type_ids'])[0].numpy().argmax().item()
 		return pred
