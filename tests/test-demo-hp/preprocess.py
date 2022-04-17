f727bf64d301049b8d9ee7a0bf95c31ca84b7b1c
Ryadh Khsib <ryadh.khsib@gmail.com>
2022-04-17 21:45:13 +0100

    Update preprocess.py

--- tests/test-demo-hp/preprocess.py
+++ tests/test-demo-hp/preprocess.py
@@ -7,6 +7,10 @@ data_path = os.environ.get('DATA_PATH') or '/tmp/data/'
 if not os.path.exists(data_path):
 	os.makedirs(data_path)
 
+	
+assert os.environ.get('SENSOR_PARAM1') == "SENSOR_PARAM1_VALUE"
+assert os.environ.get('SENSOR_PARAM2') == "SENSOR_PARAM2_VALUE"
+
 df = pd.DataFrame(np.random.randn(100, 4), columns=list('ABCD'))
 df.to_pickle(data_path + "example.pkl")
 
